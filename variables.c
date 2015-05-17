228b6f79b5691901c733350f97b3722cdabdf3bf
Vladimir Kirovskiy <vkirovskiy@gmail.com>
2015-05-17 19:27:32 +0600

    Added variables from main file

--- variables.c
+++ variables.c
@@ -1,2 +1,10 @@
 #define F_CPU 8000000L
 
+#define DS_PORT         PORTA
+#define DS_DDR          DDRA
+#define DS_PIN          PINA
+#define DS_CTL          5
+
+const char dsmessage[] PROGMEM = "Searching roms..";
+const char romfound[] PROGMEM = "Found: ";
+
